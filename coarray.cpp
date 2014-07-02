66b9591da43ce960e4fd06b1d8d75c7f878fefcf
Jeremy Kemp <jakemp@uh.edu>
2014-07-02 18:11:38 -0500

    need to get gasnet working for more

--- coarray.cpp
+++ coarray.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-class remote_reference {};
+
+template<typename T>
+class remote_reference {
+    public:
+        remote_reference();
+        T& operator[](int i);
+    private:
+        int size;
+};
 
 template<typename T>
 class coarray {
@@ -14,7 +22,10 @@ class coarray {
 
         void allocate();
         void deallocate();
-        remote_reference operator()(int);
+        remote_reference operator()(int){
+            //I need to set up the remote references here.
+        }
+
         //not sure if the const version is needed
         //const T& operator[](int i) const { return local_data[i];}
         T& operator[](int i){return local_data[i];}
@@ -24,13 +35,20 @@ class coarray {
         int *extents;
 };
 
+int this_image(){
+    return 0;
+}
+
+int num_images(){
+    return 1;
+}
+
 template<typename T>
 coarray<T>::coarray(int size) {
     dim = 1;
     local_data = new T[size];
     extents = new int[1];
     extents[0] = size;
-
 };
 
 int main(int argc, char **argv) 
@@ -38,5 +56,10 @@ int main(int argc, char **argv)
     coarray<int> test(10);
     test[0] = 42;
     std::cout << test[0] << std::endl;
+
+    if(this_image() == 0){
+        cout << num_images() << endl;
+    }
+    test( (this_image() + 1) % num_images())[1] = this_image()
     return 1;
 }
