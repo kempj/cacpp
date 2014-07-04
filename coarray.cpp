2cf17be2f92e0756e17a2167d6267b747d422ad9
Jeremy Kemp <jakemp@uh.edu>
2014-07-03 19:55:11 -0500

    going home. need to do puts and gets

--- coarray.cpp
+++ coarray.cpp
@@ -20,28 +20,24 @@ using std::endl;
         gasnet_exit(_retval);                                       \
     }                                                               \
 } while(0)
-/*
-void init_nodes() {
-    int num_nodes = gasnet_nodes();
-    if(!(*nodes)) {
-        *nodes = new gasnet_seginfo_t[num_nodes];
-        GASNET_SAFE(gasnet_getSegmentInfo(*nodes, num_nodes));
-    }
-}*/
 
 template<typename T>
 class coarray {
     public:
         //coarray(int dim, int *size);
         coarray(int size = 1);
+        coarray(gasnet_seginfo_t s){
+            node_info = s;
+        }
         //~coarray();//maybe object as other nodes see it to -1 
         coarray<T>& operator()(int id){
-            return remote_coarrays[id];
+            return *remote_coarrays[id];
         }
         T& operator[](int i){
             if(local_data)
                 return local_data[i];
             else {
+                //gasnet_get();
             }
         }
     private:
@@ -49,39 +45,26 @@ class coarray {
         int dim;
         int *extents;
         coarray<T> **remote_coarrays;
+        gasnet_seginfo_t node_info;
 };
-
-static gasnet_seginfo_t *nodes;
 
 template<typename T>
 coarray<T>::coarray(int size) {
-    //init_nodes();
     remote_coarrays = new coarray<T>*[gasnet_nodes()];
+    gasnet_seginfo_t *s =  new gasnet_seginfo_t[gasnet_nodes()];
+    GASNET_SAFE(gasnet_getSegmentInfo(s, gasnet_nodes()));
+    for(int i = 0; i < gasnet_nodes(); i++) {
+        if(gasnet_mynode() == i)
+            remote_coarrays[i] = this;
+        else
+            remote_coarrays[i] = new coarray<T>(s[i]);
+    }
+
     dim = 1;
     local_data = new T[size];
     extents = new int[1];
     extents[0] = size;
 };
-/*
-template<typename T>
-class remote_coarray : coarray<T>{
-    public:
-        remote_coarray();
-        T& operator[](int i){
-            return tmp;
-        }
-        T& operator=(T arg) {
-            //put remote data here
-        }
-        void init(gasnet_seginfo_t s){
-            addr = s.addr;
-        }
-    private:
-        void *addr;
-        T tmp;
-        int dim;
-        int *extents;
-};*/
 
 int this_image(){
     return gasnet_mynode();;
