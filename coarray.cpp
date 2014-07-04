d71c26bf77f1f24b7112978d5b81650ea7aec54c
Jeremy Kemp <jakemp@uh.edu>
2014-07-03 19:31:03 -0500

    been too long since last commit

--- coarray.cpp
+++ coarray.cpp
@@ -3,56 +3,8 @@
 #define GASNET_PAR 1
 #include "gasnet.h"
 
-template<typename T>
-class remote_reference {
-    public:
-        remote_reference();
-        T& operator[](int i){ return tmp;}
-    private:
-        int size;
-        T tmp;
-};
-
-template<typename T>
-class coarray {
-    public:
-        //codimensions can wait
-        //coarray(int dim, int *extents, int codim, int *coextents);
-        coarray(int dim, int *size);
-        coarray(int size);
-        coarray();
-        //        ~coarray();
-
-        void allocate();
-        void deallocate();
-        remote_reference<T> operator()(int){
-            //I need to set up the remote references here.
-        }
-
-        //not sure if the const version is needed
-        //const T& operator[](int i) const { return local_data[i];}
-        T& operator[](int i){return local_data[i];}
-    private:
-        T *local_data;
-        int dim;
-        int *extents;
-};
-
-int this_image(){
-    return gasnet_mynode();;
-}
-
-int num_images(){
-    return gasnet_nodes();
-}
-
-template<typename T>
-coarray<T>::coarray(int size) {
-    dim = 1;
-    local_data = new T[size];
-    extents = new int[1];
-    extents[0] = size;
-};
+using std::cout;
+using std::endl;
 
 /* Macro to check return codes and terminate with useful message. */
 #define GASNET_SAFE(fncall) do {                                    \
@@ -68,34 +20,99 @@ coarray<T>::coarray(int size) {
         gasnet_exit(_retval);                                       \
     }                                                               \
 } while(0)
+/*
+void init_nodes() {
+    int num_nodes = gasnet_nodes();
+    if(!(*nodes)) {
+        *nodes = new gasnet_seginfo_t[num_nodes];
+        GASNET_SAFE(gasnet_getSegmentInfo(*nodes, num_nodes));
+    }
+}*/
+
+template<typename T>
+class coarray {
+    public:
+        //coarray(int dim, int *size);
+        coarray(int size = 1);
+        //~coarray();//maybe object as other nodes see it to -1 
+        coarray<T>& operator()(int id){
+            return remote_coarrays[id];
+        }
+        T& operator[](int i){
+            if(local_data)
+                return local_data[i];
+            else {
+            }
+        }
+    private:
+        T *local_data;
+        int dim;
+        int *extents;
+        coarray<T> **remote_coarrays;
+};
+
+static gasnet_seginfo_t *nodes;
+
+template<typename T>
+coarray<T>::coarray(int size) {
+    //init_nodes();
+    remote_coarrays = new coarray<T>*[gasnet_nodes()];
+    dim = 1;
+    local_data = new T[size];
+    extents = new int[1];
+    extents[0] = size;
+};
+/*
+template<typename T>
+class remote_coarray : coarray<T>{
+    public:
+        remote_coarray();
+        T& operator[](int i){
+            return tmp;
+        }
+        T& operator=(T arg) {
+            //put remote data here
+        }
+        void init(gasnet_seginfo_t s){
+            addr = s.addr;
+        }
+    private:
+        void *addr;
+        T tmp;
+        int dim;
+        int *extents;
+};*/
+
+int this_image(){
+    return gasnet_mynode();;
+}
+
+int num_images(){
+    return gasnet_nodes();
+}
 
 int main(int argc, char **argv) 
 {
-    size_t segsz = GASNET_PAGESIZE;
-    size_t heapsz = GASNET_PAGESIZE;
-
     GASNET_SAFE(gasnet_init(&argc, &argv));
-
-    GASNET_SAFE(gasnet_attach(NULL, 0, segsz, heapsz));
+    GASNET_SAFE(gasnet_attach(NULL, 0, GASNET_PAGESIZE, GASNET_PAGESIZE));
 
     int id = this_image();
     int team_size = num_images();
-
     coarray<int> test(team_size);
 
-    if(this_image() == 0){
+    cout << "hello from node " << id << endl;
+
+    if(id == 0){
         std::cout << num_images() << std::endl;
     }
     for(int i = 0; i < team_size; i++) {
-        test( (id+i)%team_size )[i] = team_size * id + i; 
+        test( (id+i) % team_size)[i] = team_size * id + i; 
     }
     if(0 == id) {
         std::cout << test[0] << std::endl;
     }
-
-
-    gasnet_barrier_notify(0,GASNET_BARRIERFLAG_ANONYMOUS);
-    gasnet_barrier_wait(0,GASNET_BARRIERFLAG_ANONYMOUS);
+    //gasnet_barrier_notify(0,GASNET_BARRIERFLAG_ANONYMOUS);
+    //gasnet_barrier_wait(0,GASNET_BARRIERFLAG_ANONYMOUS);
 
     gasnet_exit(0);
 
