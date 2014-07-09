1d72ea221c0468f035c2a86292cb0148b2e4ebca
Jeremy Kemp <jakemp@uh.edu>
2014-07-09 11:10:26 -0500

    renaming driver file

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	mpicxx coarray.cpp -o cacpp -I../../packages/gasnet/include/ -I../../packages/gasnet/include/mpi-conduit/ -L../../packages/gasnet/lib/ -lpthread -lgasnet-mpi-par -lammpi -lrt
+	mpicxx driver.cpp -o cacpp -I../../packages/gasnet/include/ -I../../packages/gasnet/include/mpi-conduit/ -L../../packages/gasnet/lib/ -lpthread -lgasnet-mpi-par -lammpi -lrt
---
+++ driver.cpp
@@ -0,0 +1,29 @@
+#include "coarray.h"
+
+int main(int argc, char **argv) 
+{
+    GASNET_SAFE(gasnet_init(&argc, &argv));
+    GASNET_SAFE(gasnet_attach(NULL, 0, GASNET_PAGESIZE, GASNET_PAGESIZE));
+
+    int id = this_image();
+    int team_size = num_images();
+    coarray<int,1> test(team_size);
+
+    cout << "hello from node " << id << endl;
+
+    if(id == 0){
+        std::cout << num_images() << std::endl;
+    }
+    for(int i = 0; i < team_size; i++) {
+        test( (id+i) % team_size)[i] = team_size * id + i; 
+    }
+    if(0 == id) {
+        std::cout << test[0] << std::endl;
+    }
+    //gasnet_barrier_notify(0,GASNET_BARRIERFLAG_ANONYMOUS);
+    //gasnet_barrier_wait(0,GASNET_BARRIERFLAG_ANONYMOUS);
+
+    gasnet_exit(0);
+
+    return 1;
+}
