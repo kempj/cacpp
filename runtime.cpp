88108ecb884783ac997889148e0fd6bf6234c5f1
Jeremy Kemp <jakemp@uh.edu>
2014-08-13 18:30:05 -0500

    notes and plans

---
+++ runtime.cpp
@@ -0,0 +1,6 @@
+#include "runtime.h"
+
+cacpp_runtime::put(void *src, void *dest, size_t size, size_t node){
+
+}
+
--- runtime.h
+++ runtime.h
@@ -7,6 +7,8 @@ class cacpp_runtime {
 
     private:
         vector<coarray> coarrays;//How do I make this work?
+        //It doesn't need to be here if I use memory addresses.
+        //actually it does. Where am I going to look up remote addresses?
         gasnet_seginfo_t *segment_info;
         int image_num = -1;
         int64_t data_size=0;//Needed globally to keep track of the beginning of each new coarray
