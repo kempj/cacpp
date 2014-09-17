538d5d0116d027267fd86658869ec01202c88516
Jeremy Kemp <jakemp@uh.edu>
2014-09-17 14:47:19 -0500

    added command line parameters to test

--- benchmarks/mmult-dist.cpp
+++ benchmarks/mmult-dist.cpp
@@ -2,7 +2,7 @@
 //#include "local_array.h"
 #include <chrono>
 
-const int size = 512;
+int size = 512;
 size_t num_rows;
 size_t last_num_rows;
 
@@ -37,6 +37,44 @@ void comult2D( coarray<int, 2> A,
         row_start = id * num_rows;
         row_end = (id + 1) * num_rows;
     }
+    
+    for(int row = 0; row < section_size; row++) {
+        if(id == 0) {
+            if (row % 8 == 0){
+                cout << "row " << row << "( node " << id << ")" << endl;
+            }
+        }
+        for(int col = 0; col < size; col++) {
+            C[row][col] = 0;
+            for(int CA = 0; CA < tot-1; CA++) {
+                for(size_t inner = 0; inner < num_rows; inner++) {
+                    C[row][col] = C[row][col] + A[row][inner + CA*num_rows] * B(CA)[inner][col];
+                }
+            }
+            for(size_t inner = 0; inner < last_num_rows; inner++) {
+                C[row][col] = C[row][col] + A[row][inner + (tot-1)*num_rows] * B(tot-1)[inner][col];
+            }
+        }
+    }
+    cout << "node complete: " << id << endl;
+}
+
+void comult_LA( coarray<int, 2> A,
+               coarray<int, 2> B,
+               coarray<int, 2> C){
+    int id = this_image();
+    int tot = num_images();
+    int section_size = num_rows;
+    int row_start, row_end;
+    if(id == tot - 1) {
+        section_size = last_num_rows;
+        row_start = size - last_num_rows;
+        row_end = size;
+    } else {
+        section_size = num_rows;
+        row_start = id * num_rows;
+        row_end = (id + 1) * num_rows;
+    }
 
     local_array<int> tmp(last_num_rows);
     
@@ -51,32 +89,15 @@ void comult2D( coarray<int, 2> A,
             for(int CA = 0; CA < tot-1; CA++) {
                 tmp = B(CA)[range()][col];
                 for(size_t inner = 0; inner < num_rows; inner++) {
-                    /*
-                    for(size_t i = 0; i < tot; i++){
-                        sync_all();
-                        if(id == i) {
-                            cout << "C(" << CA << ")[" << row << "][" << col << "] += " 
-                                 << "A(" << CA <<" )[" << row << "][" << inner+CA*num_rows 
-                                 << "] * tmp[" << inner << "]" << endl;
-                            cout << "(" << C[row][col] << " += " << A[row][inner+CA*num_rows] 
-                                 << " * " << tmp[inner] << endl;
-                        }
-                    }*/
                     C[row][col] = C[row][col] + A[row][inner+CA*num_rows] * tmp[inner];
-                    //C[row][col] = C[row][col] + A[row][inner + CA*num_rows] * B(CA)[inner][col];
                 }
             }
             tmp = B(tot-1)[range()][col];
             for(size_t inner = 0; inner < last_num_rows; inner++) {
                 C[row][col] = C[row][col] + A[row][inner + (tot-1)*num_rows] * tmp[inner];
-                //C[row][col] = C[row][col] + A[row][inner + (tot-1)*num_rows] * B(tot-1)[inner][col];
             }
         }
     }
-    //for(size_t i = 0; i < last_num_rows; i++) {
-    //    cout << "tmp[" << i << "] = " << tmp[i] << endl;
-    //}
-    sync_all();
     cout << "node complete: " << id << endl;
 }
 
@@ -110,8 +131,19 @@ void printCA(coarray<int, 2> C){
 
 int main(int argc, char **argv) 
 {
-    //coarray_init(4*8*size*size, argc, argv);
-    coarray_init(3*8*1024*1024, argc, argv);
+    int run_type = 0;
+    if(argc >= 2)
+        size = atoi(argv[1]);
+    if(argc >= 3)
+        run_type = atoi(argv[2]);
+
+    cout << "run size = " << size << ", run type = " << run_type << endl;
+
+    if(size * size > 4096){
+        coarray_init(4*8*size*size, argc, argv);
+    } else {
+        coarray_init(3*8*1024*1024, argc, argv);
+    }
 
     size_t id = this_image();
     size_t team_size = num_images();
@@ -129,7 +161,11 @@ int main(int argc, char **argv)
 
     std::chrono::time_point <std::chrono::system_clock> start, stop, stop2;
     start= std::chrono::system_clock::now();
-    comult2D(A, B, C);
+    if(run_type == 0) {
+        comult2D(A, B, C);
+    } else {
+        comult_LA(A, B, C);
+    }
 
     stop = std::chrono::system_clock::now();
 
